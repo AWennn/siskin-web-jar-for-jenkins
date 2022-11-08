c3d7cfed6c5a67172e1bc9dcc48ca7bffce4357a
AWennn <1660023930@qq.com>
2022-11-09 00:25:10 +0800

    update

--- src/main/java/com/siskin/config/SpringMvcConfig.java
+++ src/main/java/com/siskin/config/SpringMvcConfig.java
@@ -2,16 +2,20 @@ package com.siskin.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class SpringMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*/**")
+        registry.addMapping("/**")
+                .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowedMethods("*")
-                .maxAge(1800)
-                .allowedOrigins("http://localhost:8080");
+                .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH")
+                .maxAge(3600);
     }
 }
+
+
